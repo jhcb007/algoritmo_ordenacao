9341f5690e6f28c8bef2b39b4d4a58237d337347
henrique <jhcb007@gmail.com>
2017-10-27 07:58:57 -0300

    nova versao

--- app/app.js
+++ app/app.js
@@ -3,9 +3,9 @@
 
 var array_numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
 var tempo = 1500;
-var calculo = {
-    num1: null,
-    num2: null
+var _calculo = {
+    menor: null,
+    maior: null
 };
 
 function espera(milliseconds) {
@@ -37,38 +37,74 @@ function embaralhar() {
 }
 
 
-var executa_animacao = function () {
+var executa_animacao = function (temp_i) {
 
     for (var i = 0; i < array_numeros.length; ++i) {
-        $("#calculo_num1").html('');
-        $("#calculo_maior_menor").html('');
-        $("#calculo_num2").html('');
-        $("#calculo_num1").append('<img width="80px"  id="cal1_img_' + array_numeros[i] + '" class="rounded-circle" style="display: none" src="img/' + array_numeros[i] + '.svg">');
-        if (array_numeros[i] > array_numeros[i + 1]) {
-            $("#calculo_maior_menor").append('<img width="80px" id="menor_maior_img" class="rounded-circle" style="display: none" src="img/right.svg">');
-        } else {
-            $("#calculo_maior_menor").append('<img width="80px" id="menor_maior_img" class="rounded-circle" style="display: none" src="img/left.svg">');
-        }
-        $("#calculo_num2").append('<img width="80px" id="cal2_img_' + array_numeros[i + 1] + '" class="rounded-circle"  style="display: none" src="img/' + array_numeros[i + 1] + '.svg">');
 
-        $("#cal1_img_" + array_numeros[i]).fadeIn(200);
-        $("#menor_maior_img").fadeIn(2000);
-        $("#cal2_img_" + array_numeros[i + 1]).fadeIn(200);
-        embaralhar();
-        setTimeout(executa_animacao, 400);
-        return;
+        if (i === temp_i) {
+            $("#numero_" + array_numeros[i]).fadeOut(400);
+            $("#numero_" + array_numeros[i + 1]).fadeOut(400);
+
+            $("#calculo_num1").html('');
+            $("#calculo_maior_menor").html('');
+            $("#calculo_num2").html('');
+            $("#calculo_num1").append('<img width="80px"  id="cal1_img_' + array_numeros[i] + '" class="rounded-circle" style="display: none" src="img/' + array_numeros[i] + '.svg">');
+            if (array_numeros[i] > array_numeros[i + 1]) {
+                $("#calculo_maior_menor").append('<img width="80px" id="menor_maior_img" class="rounded-circle" style="display: none" src="img/right.svg">');
+            } else {
+                $("#calculo_maior_menor").append('<img width="80px" id="menor_maior_img" class="rounded-circle" style="display: none" src="img/left.svg">');
+            }
+            $("#calculo_num2").append('<img width="80px" id="cal2_img_' + array_numeros[i + 1] + '" class="rounded-circle"  style="display: none" src="img/' + array_numeros[i + 1] + '.svg">');
+
+            $("#cal1_img_" + array_numeros[i]).fadeIn(200);
+            $("#menor_maior_img").fadeIn(2000);
+            $("#cal2_img_" + array_numeros[i + 1]).fadeIn(200);
+        }
+
+
+        //embaralhar();
+        //setTimeout(executa_animacao(temp_i + 1), tempo);
     }
+
 
 };
 
 
+function selectionSort(items) {
+    var length = items.length;
+    for (var i = 0; i < length - 1; i++) {
+        var min = i;
+        for (var j = i + 1; j < length; j++) {
+            if (items[j] < items[min]) {
+                min = j;
+
+            }
+        }
+        if (min !== i) {
+            var tmp = items[i];
+            items[i] = items[min];
+            items[min] = tmp;
+        }
+    }
+}
+
+function calc_teste() {
+    console.log(array_numeros);
+    selectionSort(array_numeros);
+    console.log(array_numeros);
+}
+
+
 function animacao() {
-    executa_animacao();
+    executa_animacao(0);
+    //calc_teste();
 }
 
 
 function init() {
     embaralhar();
+    popular_numeros();
+    exibir_numeros(false);
 
 }
 
